dd4204eebe168b5febd49f6a0590f30ffe9d1edf
Hyun <107024710+haden-hyun@users.noreply.github.com>
2026-03-31 10:28:32 +0900

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -24,13 +24,11 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.ConditionalRender({
-      component: Component.Breadcrumbs(),
-      condition: (page) => page.fileData.slug !== "index",
-    }),
+    Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
+    RecentNotesForIndex,
   ],
   left: [
     Component.PageTitle(),
