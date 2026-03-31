3d1fcf1977d1cf9d532b1eec3c941c1725986b6a
hayden-hyun <haejun6597@naver.com>
2026-03-31 21:43:32 +0900

    fix: layout.tx

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -1,6 +1,6 @@
 import { PageLayout, SharedLayout } from "./quartz/cfg"
 import * as Component from "./quartz/components"
-import RecentNotesForIndex from "./quartz/components/RecentNotesForIndex"
+// import RecentNotesForIndex from "/.quartz/components/RecentNotesForIndex"
 
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
@@ -25,17 +25,28 @@ export const sharedPageComponents: SharedLayout = {
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
-    Component.Breadcrumbs(),
+    Component.ConditionalRender({
+      component: Component.Breadcrumbs(),
+      condition: (page) => page.fileData.slug !== "index",
+    }),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
-    RecentNotesForIndex,
   ],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),    
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+        { Component: Component.ReaderMode() },
+      ],
+    }),
+    Component.Explorer(),
   ],
   right: [
     Component.Graph(),
@@ -50,8 +61,16 @@ export const defaultListPageLayout: PageLayout = {
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),    
+    Component.Flex({
+      components: [
+        {
+          Component: Component.Search(),
+          grow: true,
+        },
+        { Component: Component.Darkmode() },
+      ],
+    }),
+    Component.Explorer(),
   ],
   right: [],
 }
--- quartz/components/RecentNotesForIndex.tsx
+++ quartz/components/RecentNotesForIndex.tsx
@@ -1,17 +1,20 @@
-// RecentNotesForIndex.tsx
 import { QuartzComponent, QuartzComponentProps } from "./types"
 import RecentNotes from "./RecentNotes"
- 
-const RecentNotesForIndex: QuartzComponent = ({ fileData, ...props }: QuartzComponentProps) => {
-  return fileData.slug === "index" ? (
-    <RecentNotes
-      title="Recent Posts"
-      limit={5}
-      showTags={true}
-      linkToMore="/all-posts"
-      {...props}
-    />
-  ) : null
+
+const RecentNotesForIndex: QuartzComponent = (props: QuartzComponentProps) => {
+  if (props.fileData.slug === "index") {
+    return RecentNotes({
+      title: "Recent Posts",
+      limit: 10,
+      showTags: true,
+      linkToMore: false,
+      filter: (f) => f.slug !== "index",
+    })(props)
+  }
+  return null
 }
- 
+
+// Ensure styles from the inner RecentNotes component are included
+RecentNotesForIndex.css = RecentNotes({}).css
+
 export default RecentNotesForIndex
